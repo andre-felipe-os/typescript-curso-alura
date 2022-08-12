b996013d20af4818241cf2e39b116e816651e480
André Sousa <andre.lipe.o@gmail.com>
2022-08-12 16:49:33 -0300

    Usa operador readonly nos atributos quantidade e valor para remover getters e modifica getter de data para evitar modificações na instância retornada por referência.

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -2,24 +2,16 @@ export class Negociacao {
 
     constructor(
         private _data: Date,
-        private _quantidade: number,
-        private _valor: number
+        public readonly quantidade: number,
+        public readonly valor: number
     ) {}
 
     get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
+        return new Date(this._data.getTime());
     }
 
     get volume(): number {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
     
 }

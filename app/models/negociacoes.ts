327002a669ee9a24e9989799d65cf6740bcf56e6
André Sousa <andre.lipe.o@gmail.com>
2022-08-11 21:19:32 -0300

    Adiciona o modelo 'negociações' que armazena e lista negociações sem permitir remoções ou modificações na lista.

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,16 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao): void {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): ReadonlyArray<Negociacao> {
+        return this.negociacoes;
+    }
+}
+
+const lista = new Negociacoes();
+lista.adiciona(new Negociacao(new Date(), 50, 125.0));

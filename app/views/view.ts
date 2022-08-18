962fe76b8b16c0e050eeef50fc1fb60a36602338
André Sousa <andre.lipe.o@gmail.com>
2022-08-18 16:15:18 -0300

    Adiciona a classe abstrata que condensa de forma genérica as propriedades e comportamentos de uma visão.

---
+++ app/views/view.ts
@@ -0,0 +1,15 @@
+export abstract class View<T> {
+
+    protected elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    abstract template(modelo: T): string;
+
+    update(modelo: T): void {
+        this.elemento.innerHTML = this.template(modelo);
+    }
+
+}

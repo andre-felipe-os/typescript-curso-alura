2072b7029fec47014e28bdf5b21386350de8d32e
André Sousa <andre.lipe.o@gmail.com>
2022-08-25 19:32:31 -0300

    Aciona a opção de passar um parâmetro que permite o desenvolvedor escolher em que unidade mostrar o tempo de execução dos métodos.

--- app/src/decorators/mostrar-tempo-de-execucao.ts
+++ app/src/decorators/mostrar-tempo-de-execucao.ts
@@ -1,15 +1,21 @@
-export function mostrarTempoDeExecucao() {
+export function mostrarTempoDeExecucao(emSegundos: boolean = true) {
     return function(
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
     ) {
+        let divisor = 1000;
+        let unidade = "segundos";
+        if (!emSegundos) {
+            divisor = 1;
+            unidade = "milisegundos";
+        }
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...args: Array<any>) {
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`Tempo de execução de '${propertyKey}': ${(t2-t1)/1000} segundos`);
+            console.log(`Tempo de execução de '${propertyKey}': ${(t2-t1)/divisor} ${unidade}`);
             return retorno;
         }
         return descriptor;

209aa1a797d75a8ffc55e2eb2a498eea8955c1c1
André Sousa <andre.lipe.o@gmail.com>
2022-08-26 09:13:34 -0300

    Torna a definição do decorator 'escape' mais padronizada, embora mais verbosa.

--- app/src/decorators/escape.ts
+++ app/src/decorators/escape.ts
@@ -1,17 +1,19 @@
-export function escape(
-    target: any,
-    propertyKey: any,
-    descriptor: PropertyDescriptor
-) {
-    const metodoOriginal = descriptor.value;
-    descriptor.value = function(...args: Array<any>) {
-        let retorno = metodoOriginal.apply(this, args);
-        console.log(`@escape em ação na classe ${this.constructor.name} `
-            + `para o método ${propertyKey}`);
-        if (typeof retorno === 'string') {
-            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+export function escape() {
+    return function(
+        target: any,
+        propertyKey: any,
+        descriptor: PropertyDescriptor
+    ) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args: Array<any>) {
+            let retorno = metodoOriginal.apply(this, args);
+            console.log(`@escape em ação na classe ${this.constructor.name} `
+                + `para o método ${propertyKey}`);
+            if (typeof retorno === 'string') {
+                retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+            }
+            return retorno;
         }
-        return retorno;
+        return descriptor;
     }
-    return descriptor;
 }

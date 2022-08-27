8c75f9a0b5c923c67168d80aa311650507435d0a
André Sousa <andre.lipe.o@gmail.com>
2022-08-27 20:22:46 -0300

    Revert "feat: cria função utilitária 'imprimir' e a utiliza ao adicionar uma nova negociação"
    This reverts commit 6217cb57e183749efffc7f9d76df614cbf03d873.

--- app/src/utils/imprimir.ts
+++
@@ -1,7 +0,0 @@
-import { Imprimivel } from "../interfaces/imprimivel.js";
-
-export function imprimir(...objetos: Array<Imprimivel>) {
-    for (let objeto of objetos) {
-        console.log(objeto.paraTexto());
-    }
-}
